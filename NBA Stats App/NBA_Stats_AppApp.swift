ff230070e7143337b75fa43b02a81b040d6c2a9e
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-03-17 14:34:40 -0500

    Initial Commit

---
+++ NBA Stats App/NBA_Stats_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_Stats_AppApp.swift
+//  NBA Stats App
+//
+//  Created by Jonathan Varghese on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NBA_Stats_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
